8c0199ce8ee901caf775d2b5090fa667e532f6bb
gunners6518 <gunners6518@gmail.com>
2020-11-14 15:37:45 +0900

    todoのcreateの実装

--- src/App.js
+++ src/App.js
@@ -1,47 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faChevronRight,
+	faChevronLeft,
+	faCircle,
+	faCheckCircle,
+	faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
-	// HINT: each "item" in our list names a name, a boolean to tell if its been completed, and a quantity
+	//itemのuseStateを作る
+	//初期状態は空のオブジェクトでOK
 	const [items, setItems] = useState([]);
 
+	//入力値のuseState作る
+	const [inputValue, setInputValue] = useState('');
+
+	//クリック時にitems配列に新しいitemを作る処理
+	const handleAddButtonClick = () => {
+
+		//作られるitemの定義
+		const newItem = {
+			itemName: inputValue,
+			quantity: 1,
+			isSelected: false,
+		};
+
+		//items配列にpushされる
+		const newItems = [...items, newItem];
+
+		//useStateのitemsに反映
+		setItems(newItems);
+
+		//入力値を空に
+		setInputValue('');
+	};
+
 	return (
-		<div className='app-background'>
-			<div className='main-container'>
-				<div className='add-item-box'>
-					<input className='add-item-input' placeholder='Add an item...' />
-					<FontAwesomeIcon icon={faPlus} />
+		<div className="app-background">
+			<div className="main-container">
+				<div className="add-item-box">
+				{/* inputValueにクリック時の入力値を与える */}
+				<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+					{/* プラスアイコンでhandleAddButtonClickを発火 */}
+					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()}/>
 				</div>
-				<div className='item-list'>
-					<div className='item-container'>
-						<div className='item-name'>
-							{/* HINT: replace false with a boolean indicating the item has been completed or not */}
-							{false ? (
-								<>
-									<FontAwesomeIcon icon={faCheckCircle} />
-									<span className='completed'>Item 1</span>
-								</>
-							) : (
-								<>
-									<FontAwesomeIcon icon={faCircle} />
-									<span>Item 1</span>
-								</>
-							)}
+				<div className="item-list">
+					{/* mapを使ってitems配列をitemのループで出力する */}
+					{items.map((item, index) => (
+						<div className="item-container">
+							<div className="item-name">
+								{item.isSelected ? (
+									<>
+										<FontAwesomeIcon icon={faCheckCircle} />
+										<span className="completed">{item.itemName}</span>
+									</>
+								) : (
+									<>
+										<FontAwesomeIcon icon={faCircle} />
+										<span>{item.itemName}</span>
+									</>
+								)}
+							</div>
+							<div className="quantity">
+								<button>
+									<FontAwesomeIcon icon={faChevronLeft} />
+								</button>
+								<span> {item.quantity} </span>
+								<button>
+									<FontAwesomeIcon icon={faChevronRight} />
+								</button>
+							</div>
 						</div>
-						<div className='quantity'>
-							<button>
-								<FontAwesomeIcon icon={faChevronLeft} />
-							</button>
-							<span> 1 </span>
-							<button>
-								<FontAwesomeIcon icon={faChevronRight} />
-							</button>
-						</div>
-					</div>
+					))}
 				</div>
-				<div className='total'>Total: 6</div>
+				<div className="total">Total: </div>
 			</div>
 		</div>
 	);
